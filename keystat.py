f87940692724ac37005c1c0c09190dd9397077e3
Юрий Махоткин <kgofhedgehogs@gmail.com>
2018-12-26 01:01:25 +0200

    начал писать каменты на русском, соре англечане

---
+++ keystat.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from os.path import expanduser
+from re import sub
+import pandas as pd
+import argparse
+
+def read_keylog(path, sep=", "):
+    return pd.read_csv(path, delimiter=sep, header=0, engine='python')
+
+def calc_press_stat(keylog): # optimize
+    keylog["iso_next_group"] = keylog["mods_mask"].map(lambda x: (int(str(x), 2)>>13) & 1)
+    data = keylog[(keylog["pressed"] == 1) & \
+                  (keylog["repeated"] == 0)] \
+           .groupby(["symbol", "iso_next_group"]) \
+           .size() \
+           .sort_values(ascending=False)
+    data = pd.DataFrame({"sm_m":data.index, "cnt":data.values})
+    data[["symbol", "iso_next_group"]] = data["sm_m"].apply(pd.Series)
+    data = data.drop(columns="sm_m")
+    data = pd.merge(data, keylog[["symbol", "repr"]], on="symbol", how="left").drop_duplicates()
+    data.fillna(value="None", inplace=True)
+    return data
+
+def write_stat(stat, path, sep=", "):
+    stat.to_csv(path, sep='\t', encoding='utf-8', index=False)
+    with open(path, "r+") as stat_f:
+      stat_s = stat_f.read()
+      stat_s = sub('\t', sep, stat_s)
+      stat_f.seek(0)
+      stat_f.write(stat_s)
+      stat_f.truncate()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Statistics aggregator for inputlistener.py')
+    parser.add_argument('-i', action='store', dest='keylog_filepath',
+                        help='keylog csv file path; default ~/.keylog.csv',
+                        default=expanduser("~")+"/.keylog.csv")
+    parser.add_argument('-o', action='store', dest='stat_filepath',
+                        help='statistics csv file path; default ~/.keystat.csv',
+                        default=expanduser("~")+"/.keystat.csv")
+    args = parser.parse_args()
+    return args.keylog_filepath, args.stat_filepath
+
+def main():
+    keylog_filepath, stat_filepath = parse_args()
+    keylog = read_keylog(keylog_filepath)
+    keylog.symbol = keylog.symbol.map(lambda x: x.upper())
+    keylog.repr = keylog.repr.map(lambda x: (lambda y: repr(y.upper()) if type(y) == str else y)(eval(x)))
+    write_stat(calc_press_stat(keylog), stat_filepath)
+
+if __name__ == "__main__":
+    main()
