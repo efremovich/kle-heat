7638cd8143f2bd7b21fbdee8520eaf8852145670
Юрий Махоткин <kgofhedgehogs@gmail.com>
2018-12-27 20:05:22 +0200

    resorted functions definition order

--- apply_stat2kle.py
+++ apply_stat2kle.py
@@ -1,11 +1,53 @@
 #!/usr/bin/python
 # -*-coding:utf-8-*-
 from os.path import expanduser
-from misc import int_rgb2tuple, try_int, list_get, format_rgb, val2rgb_gradient, comp_label, decomp_label
+from misc import int_rgb2tuple, try_int, list_get, format_rgb, val2rgb_gradient, stepped_gradient, comp_label, decomp_label
 import json
 import pandas as pd
 import numpy as np
 import argparse
+
+
+### Globals, need to plase them in settings file
+#GRADIENT_COLORS = [0x00bab4, 0xCCD100, 0xFF0000] # cayan, green, yellow, red
+#GRADIENT_COLORS = [0xcccccc,0x00FFFF, 0x00FF00, 0xFFFF00, 0xFF0000, 0xFF0000] # toxic rainbow
+#GRADIENT_COLORS = [0xcccccc,0xFEEB65, 0xE4521B, 0xcd2f2c] # pretty yellow orange orange red
+#GRADIENT_COLORS = [0xcccccc, 0xFFD100, 0xcb2f2a]          #        yellow orange red
+#GRADIENT_COLORS = [0xFFFFFF, 0x606060, 0x404040] # grayscale
+GRADIENT_COLORS = [0xcccccc, 0xffe08d, 0xf9cd31,0xf9cd31, 0xff6d1a] # mine keycaps
+GRADIENT_COLORS = list(map(int_rgb2tuple, GRADIENT_COLORS))
+
+DEFAULT_A = 4
+HAND_IDX = 9
+COUNTER_IDX = 10
+
+#LEGENDS_IDXS = [0, 2, 3, 5, 6, 8]
+LEGENDS_IDXS = [1, 4, 7]
+#LEGENDS_IDXS = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+ISO_GR_IDXS = [[0, 1, 2],
+               [3, 4, 5]]
+
+FN_IDXS = [[9,  [0, 3, 6]],
+           [11, [2, 5, 8]]]
+
+FN_NAMES2ABBREV = {
+    "LOWER"  : "l" ,
+    "LOWER_L": "ll",
+    "LOWER_R": "lr",
+    "RAISE"  : "r" ,
+    "RAISE_L": "rl",
+    "RAISE_R": "rr",
+    "FN"     : "f" ,
+    "FN1"    : "f1",
+    "FN2"    : "f2",
+    "FN3"    : "f3",
+    "FN4"    : "f4"}
+
+FN_ABBREV2NAMES = {v: k for k, v in FN_NAMES2ABBREV.items()}
+
+FN_PARAMS = {abbrev: {"i": None, "j": None, "a": None, "counter": 0}
+             for abbrev in FN_ABBREV2NAMES}
+### Globals end
 
 
 def parse_args():
@@ -67,7 +109,7 @@ def count_keypresses(layout, keystat):
                     cnt = 0
                     hand = d_p[HAND_IDX]
 
-                    for idx, k in enumerate(d_p[LEGENDS_IDXS]):
+                    for idx, k in [(idx, d_p[idx]) for idx in LEGENDS_IDXS]:
                         if k:
                             for s_k in k.split(" "):
                                 legend_cnt = 0
@@ -98,7 +140,6 @@ def count_keypresses(layout, keystat):
                                     FN_PARAMS[abbrev]["j"] = j
                                     FN_PARAMS[abbrev]["a"] = a
 
-
                     c = try_int(list_get(d_p, COUNTER_IDX, 0))
                     d_p[COUNTER_IDX] = cnt + c
                     layout[i][j] = comp_label(a, d_p)
@@ -111,6 +152,7 @@ def count_keypresses(layout, keystat):
             d_p[COUNTER_IDX] = counter + c
             layout[i][j] = comp_label(a, d_p)
     return layout
+
 
 def calc_min_max_keypresses(layout, keystat):
     minval = keystat.cnt.min()
@@ -125,11 +167,11 @@ def calc_min_max_keypresses(layout, keystat):
     #                a = p.get('a', a)
     #            elif isinstance(p, str):
     #                d_p = decomp_label(a, p)
-
     #                c = try_int(list_get(d_p, COUNTER_IDX, 0))
     #                minval = min(c, minval) if minval is not None else c
     #                maxval = max(c, maxval) if maxval is not None else c
     return minval, maxval
+
 
 def color_keys(layout, minval, maxval):
     a = DEFAULT_A
@@ -148,13 +190,13 @@ def color_keys(layout, minval, maxval):
                     c = try_int(list_get(d_p, COUNTER_IDX, 0))
                     #col = format_rgb(
                     #    stepped_gradient(
-                    #        minval, maxval, c, gradient_colors))
+                    #        minval, maxval, c, GRADIENT_COLORS))
                     col = format_rgb(
                         val2rgb_gradient(
-                            minval, maxval, c, gradient_colors))
+                            minval, maxval, c, GRADIENT_COLORS))
                     #col = format_rgb(
                     #    val2rgb_gradient(
-                    #        0, count_keys, cntr, gradient_colors))
+                    #        0, count_keys, cntr, GRADIENT_COLORS))
                     #norm_c = constrain(0, 1, cntr/count_keys)
                     #col = format_rgb(cubehelix(0,1,1,0.8,norm_c))
                     layout[i].insert(j, {"c": col})
@@ -162,46 +204,6 @@ def color_keys(layout, minval, maxval):
                     cntr += 1
     return layout
 
-### Globals, need to plase them in settings file
-
-#gradient_colors = [0x00bab4, 0xCCD100, 0xFF0000] # cayan, green, yellow, red
-#gradient_colors = [0xcccccc,0x00FFFF, 0x00FF00, 0xFFFF00, 0xFF0000, 0xFF0000] # toxic rainbow
-gradient_colors = [0xcccccc,0xFEEB65, 0xE4521B, 0xcd2f2c] # pretty yellow orange orange red
-#gradient_colors = [0xcccccc, 0xFFD100, 0xcb2f2a]          #        yellow orange red
-#gradient_colors = [0xFFFFFF, 0x606060, 0x404040] # grayscale
-#gradient_colors = [0xcccccc, 0xffe08d, 0xf9cd31,0xf9cd31, 0xff6d1a] # mine keycaps
-gradient_colors = list(map(int_rgb2tuple, gradient_colors))
-
-DEFAULT_A = 4
-HAND_IDX = 9
-COUNTER_IDX = 10
-
-LEGENDS_IDXS = slice(0, 9)
-ISO_GR_IDXS = [[0, 1, 2],
-               [3, 4, 5]]
-
-FN_IDXS = [[9,  [0, 3, 6]],
-           [11, [2, 5, 8]]]
-
-FN_NAMES2ABBREV = {
-    "LOWER"  : "l" ,
-    "LOWER_L": "ll",
-    "LOWER_R": "lr",
-    "RAISE"  : "r" ,
-    "RAISE_L": "rl",
-    "RAISE_R": "rr",
-    "FN"     : "f" ,
-    "FN1"    : "f1",
-    "FN2"    : "f2",
-    "FN3"    : "f3",
-    "FN4"    : "f4"}
-
-FN_ABBREV2NAMES = {v: k for k, v in FN_NAMES2ABBREV.items()}
-
-FN_PARAMS = {abbrev: {"i": None, "j": None, "a": None, "counter": 0}
-             for abbrev in FN_ABBREV2NAMES}
-
-### Globals end
 
 def main():
     stat_path, layout_path, output_path = parse_args()
